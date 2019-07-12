379698a2cfdc28db9d65c156b1681ffd669651f2
d <krex>
2019-07-12 15:06:21 +0200

    admin od kreke

--- src/components/AdminOffers.vue
+++ src/components/AdminOffers.vue
@@ -80,4 +80,226 @@ export default {
 </script>
 
 
+<style scoped>
+
+.offerBtn {
+  display: block;
+  padding: 10px;
+  font-size: 12px;
+  border-radius: 3px;
+  font-weight: bold;
+  margin-top: 2px;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+.offerBtn.green{
+  background-color: #05A701;
+  color: white !important;
+}
+
+.offerBtn.orange{
+  background-color:rgba(255,122,0,0.3);
+  color: #FF7A00 !important;
+  border: 2px solid #FF7A00;
+  box-sizing: border-box;
+}
+
+.offerBtn.blue{
+  background-color: #00386C;
+  color: white !important;
+}
+
+.offerBtn.lightBlue{
+  color: #00386C !important;
+  border: 2px solid #00386C;
+  box-sizing: border-box;
+}
+
+.offerBtn:hover{
+  opacity: 0.8;
+}
+
+.txtHolder{
+  width: 58%;
+  text-align: left;
+}
+
+.txtHolder.big{
+  width: 78%;
+  text-align: left;
+}
+
+.txtHolder h4{
+  font-size: 16px;
+}
+
+.txtHolder p{
+  font-size: 12px;
+  line-height: 1.2;
+}
+
+.imgHolder{
+  width: 20%;
+}
+
+.buttonHolder{
+  width: 20%;
+}
+
+.imageHolder img {
+  height: 80px;
+  width: 80px;
+  object-fit: cover;
+}
+
+.offer {
+  width: 100%;
+  background-color: white;
+  border: 1px solid lightgrey;
+  border-radius: 5px;
+  display: flex;
+  padding: 20px;
+  justify-content: space-between;
+  margin-top: 10px; 
+}
+
+.offer:first-of-type {
+  margin-top: 25px; 
+}
+
+.offer:last-of-type {
+  margin-bottom: 50px; 
+}
+
+.catHolder {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.catSingle {
+  display: inline-block;
+  width: 8.333%;
+  min-width: 60px;
+  cursor: pointer;
+}
+
+.catSingle img {
+  width: 100%;
+}
+
+.catSingle span {
+  display: block;
+  font-size: 11px !important;
+  padding: 10px;
+  line-height: 1.2;
+}
+
+.catBorder {
+  border-bottom: 5px solid;
+  box-sizing: border-box;
+}
+
+.cat1 {
+  border-color: #cb2422;
+}
+
+.cat2 {
+  border-color: #d26b13;
+}
+
+.cat3 {
+  border-color: #fac401;
+}
+
+.cat4 {
+  border-color: #afc624;
+}
+
+.cat5 {
+  border-color: #829427;
+}
+
+.cat6 {
+  border-color: #43b17a;
+}
+
+.cat7 {
+  border-color: #419cd6;
+}
+
+.cat8 {
+  border-color: #283476;
+}
+
+.cat9 {
+  border-color: #55d6ff;
+}
+
+.cat10 {
+  border-color: #a8529a;
+}
+
+.cat11 {
+  border-color: #e84490;
+}
+
+.cat12 {
+  border-color: #dcdcdc;
+}
+
+.cat1.active {
+  background-color: #cb2422;
+}
+
+.cat2.active {
+  background-color: #d26b13;
+}
+
+.cat3.active {
+  background-color: #fac401;
+}
+
+.cat4.active {
+  background-color: #afc624;
+}
+
+.cat5.active {
+  background-color: #829427;
+}
+
+.cat6.active {
+  background-color: #43b17a;
+}
+
+.cat7.active {
+  background-color: #419cd6;
+}
+
+.cat8.active {
+  background-color: #283476;
+}
+
+.cat9.active {
+  background-color: #55d6ff;
+}
+
+.cat10.active {
+  background-color: #a8529a;
+}
+
+.cat11.active {
+  background-color: #e84490;
+}
+
+.cat12.active {
+  background-color: #dcdcdc;
+}
+
+</style>
+
+
+
+
+
 
---
+++ src/components/AdminPreview.vue
@@ -0,0 +1,269 @@
+<template>
+    <div class="preview">
+      <div class="closeBtn" @click="closeComponent()">
+        <img src="@/assets/icon-b-close.png" />
+      </div>
+
+      <div class="contentHolder">
+      <div class="buttonHolder">
+        <div>
+          <a class="offerBtn blue" @click="closeComponent()">POVRATAK</a>
+        </div>
+      <div>
+          <a class="offerBtn orange">ZATRAŽI PROMJENE</a>
+          <a class="offerBtn green">OBJAVI PONUDU</a>
+      </div>
+      </div>
+        <div class="grid-container orderInfo">
+          <div class="grid-item table1">
+            <span>POSLOVNI SUBJEKT</span>
+            <p>Adria Camping</p>
+          </div>
+          <div class="grid-item table2">
+            <span>MJESTO</span>
+            <p>Adria Camping</p>
+          </div>
+          <div class="grid-item table3">
+            <span>TELEFON</span>
+            <p>Adria Camping</p>
+          </div>
+          <div class="grid-item table4">
+            <span>E-MAIL</span>
+            <p>Adria Camping</p>
+          </div>
+          <div class="grid-item table5">
+            <span>WEB ADRESA</span>
+            <p>Adria Camping</p>
+          </div>
+          <div class="grid-item table6">
+            <span>DATOTEKA</span>
+            <p>Adria Camping</p>
+          </div>
+        </div>
+        <div class="grid-container1 orderInfo">
+          <div class="grid-item table1">
+            <span>GLAVNA KATEGORIJA</span>
+            <p>Adria Camping</p>
+          </div>
+          <div class="grid-item table2">
+            <span>PODKATEGORIJA</span>
+            <p>Adria Camping</p>
+          </div>
+          <div class="grid-item table3">
+            <span>OPIS PONUDE</span>
+            <p>Adria Camping</p>
+          </div>
+          <div class="grid-item table4">
+            <span>NAPOMENA</span>
+            <p>Adria Camping</p>
+          </div>
+        </div>
+        <div class="mainImage">
+          <span class="sectionLabel">GLAVNA FOTOGRAFIJA</span>
+          <img src="@/assets/slider1-image.jpg">
+        </div>
+        <div class="moreImage">
+          <span class="sectionLabel">DODATNE FOTOGRAFIJE</span>
+          <div class="moreImageHolder">
+          <img src="@/assets/slider1-image.jpg">
+          <img src="@/assets/slider1-image.jpg">
+          <img src="@/assets/slider1-image.jpg">
+          <img src="@/assets/slider1-image.jpg">
+          <img src="@/assets/slider1-image.jpg">
+          <img src="@/assets/slider1-image.jpg">
+          </div>
+        </div>
+
+        <div class="grid-container1 orderInfo">
+          <div class="grid-item table1">
+            <span>ADMIN</span>
+            <p>Adria Camping</p>
+          </div>
+          <div class="grid-item table2">
+            <span>DATUM</span>
+            <p>Adria Camping</p>
+          </div>
+          <div class="grid-item comment3">
+            <span>OPIS PONUDE</span>
+            <p>Adria Camping</p>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "AdminPreview",
+  components: {},
+  props: {},
+  methods: {
+    closeComponent() {
+        this.$emit('closePreview')
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* --- START PREVIEW --- */
+.buttonHolder{
+  display: flex;
+  justify-content: space-between;
+  margin-top: 50px;
+}
+
+.offerBtn {
+  display: inline-block;
+  padding: 10px;
+  font-size: 12px;
+  border-radius: 3px;
+  font-weight: bold;
+  margin-top: 2px;
+  cursor: pointer;
+  transition: 0.3s;
+  width: 200px;
+}
+
+.offerBtn.green{
+  background-color: #05A701;
+  color: white !important;
+  border: 2px solid #05A701;
+  margin-left: 5px;
+}
+
+.offerBtn.orange{
+  background-color:rgba(255,122,0,0.3);
+  color: #FF7A00 !important;
+  border: 2px solid #FF7A00;
+  box-sizing: border-box;
+}
+
+.offerBtn.blue{
+  background-color: #00386C;
+  color: white !important;
+  border: 2px solid #00386C;
+}
+
+.offerBtn.lightBlue{
+  color: #00386C !important;
+  border: 2px solid #00386C;
+  box-sizing: border-box;
+}
+
+.offerBtn:hover{
+  opacity: 0.8;
+}
+
+.moreImageHolder{
+  display: flex;
+
+  overflow-x: scroll;
+}
+
+.comment3{
+  grid-column: 1 / 3;
+}
+
+.mainImage{
+  width: 100%;
+  text-align: left;
+}
+
+.moreImage{
+  text-align: left;
+  margin-bottom: 20px;
+  width: 100%;
+}
+
+.moreImage img{
+  height: 200px;
+  margin-right: 2px;
+}
+
+.sectionLabel {
+  font-size: 12px;
+  font-weight: bold;
+  color: #00386C;
+  padding: 0 0 0 30px;
+  margin-bottom: 10px;
+  margin-top: 20px;
+  display: block;
+}
+
+.mainImage img{
+  width: 100%;
+}
+
+.grid-container {
+  display: grid;
+  grid-template-columns: auto auto auto;
+	border: 1px solid rgba(0,0,0,0.2);
+  background-color: rgba(0,0,0,0.2);
+	grid-gap: 1px;
+	justify-self: center;
+	height: auto;
+	margin: 30px 0;
+}
+
+.grid-container1 {
+  display: grid;
+  grid-template-columns: auto auto;
+	border: 1px solid rgba(0,0,0,0.2);
+  background-color: rgba(0,0,0,0.2);
+	grid-gap: 1px;
+	justify-self: center;
+	height: auto;
+	margin: 30px 0;
+}
+
+.grid-item {
+  background-color: rgba(255, 255, 255, 1);
+  padding: 15px 30px;
+  text-align: left;
+}
+
+.grid-item span {
+  font-size: 12px;
+  font-weight: bold;
+  color: #00386C;
+}
+
+.grid-item p {
+  color: #494949;
+}
+
+.contentHolder {
+  width: 80%;
+  margin: auto;
+  height: 300px;
+}
+
+.closeBtn {
+  width: 30px;
+  height: 30px;
+  position: fixed;
+  right: 35px;
+  top: 20px;
+  cursor: pointer;
+}
+
+.closeBtn img {
+  width: 100%;
+}
+
+.preview {
+  width: 100vw;
+  height: 100vh;
+  margin: 0px;
+  background-color: white;
+  position: fixed;
+  top: 0px;
+  z-index: 99999;
+  overflow-y: scroll;
+}
+
+/* --- END PREVIEW --- */
+</style>
+
+
